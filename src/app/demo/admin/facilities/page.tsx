61f857c6c8fdd8c6f500e14a9c5839d2056ad5c2
iamunguardable10-svg <iamunguardable10@gmail.com>
2026-05-11 15:55:23 +0200

    feat: add local demo admin facilities route

---
+++ src/app/demo/admin/facilities/page.tsx
@@ -0,0 +1,5 @@
+import { DemoAdminFacilitiesManager } from '@/features/admin/DemoAdminFacilitiesManager';
+
+export default function DemoAdminFacilitiesPage() {
+  return <DemoAdminFacilitiesManager />;
+}
